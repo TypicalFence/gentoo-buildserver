ae0ba0a56d2b9865349ce97cc0dce01d636ab654
fence <fence@desu-mail.moe>
2020-09-15 22:04:13 +0200

    add manage cli

--- manage.py
+++ manage.py
@@ -1,10 +1,46 @@
 #!/usr/bin/env python3
+import os
+import time
+import click
 import docker
-client = docker.from_env()
-container = client.containers.run("buildserver", "bash -c \"emerge ufetch && ufetch\"", detach=True)
 
-log_stream = container.logs(stream=True)
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+docker_client = docker.from_env()
 
-for line in log_stream:
-    print(line.decode("utf-8"))
 
+def get_voulmes(profile):
+    base = "{0}/.build_data/{1}".format(SCRIPT_DIR, profile)
+    return {
+        base + "/binpkgs": {"bind": "/var/cache/binpkgs", "mode": "rw"},
+        base + "/repos": {"bind": "/var/db/repos", "mode": "rw"},
+    }
+
+
+def run_job(name, profile):
+    moment = int(time.time())
+    docker_client.containers.run(
+        "buildserver",
+        "/root/{0} {1}".format(name, profile),
+        volumes=get_voulmes(profile),
+        name="{0}-{1}-{2}".format(profile, name, moment),
+        labels={"domain": "gentoo-build-job",
+                "started": moment, "profile": profile, "job": name},
+        detached=True
+    )
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("profile")
+def sync(profile):
+    run_job("sync", profile)
+
+
+@cli.command()
+@click.argument("profile")
+def build(profile):
+    run_job("build", profile)
